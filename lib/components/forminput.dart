43e352179c15406ffbf183afecfd57258046c29e
Buteya <buteyamasira@gmail.com>
2024-07-17 19:03:13 +0300

    signupscreen design upgrade

--- lib/components/forminput.dart
+++ lib/components/forminput.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class FormInput extends StatelessWidget {
   const FormInput({
     super.key,
+    required this.obscureText,
     required this.labelText,
     required this.hintText,
     required this.validationFunction,
@@ -13,10 +14,12 @@ class FormInput extends StatelessWidget {
   final String? labelText;
   final String? hintText;
   final Function validationFunction;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obscureText,
       controller: _usernameController,
       decoration:  InputDecoration(
         labelText: labelText,
--- lib/screens/auth/signupscreen.dart
+++ lib/screens/auth/signupscreen.dart
@@ -13,6 +13,7 @@ class _SignupScreenState extends State<SignupScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +26,48 @@ class _SignupScreenState extends State<SignupScreen> {
               inputController: _usernameController,
               labelText: 'Username',
               hintText: 'Enter your username',
-              validationFunction: (value){
-                if (value == null || value.isEmpty)
-                {
-                return 'Please enter some text';
+              validationFunction: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
                 }
-                },
+              },
+              obscureText: false,
             ),
             FormInput(
               inputController: _emailController,
               labelText: 'Email',
               hintText: 'Enter your email',
-              validationFunction: (value){
-                if (value == null || value.isEmpty)
-                {
-                  return 'Please enter some text';
+              validationFunction: (value) {
+                if (value == null || value.isEmpty) {
+                  return "please enter your email";
+                } else if (!value.contains("@")) {
+                  return "please enter a valid email";
                 }
               },
+              obscureText: false,
+            ),
+            FormInput(
+              obscureText: false,
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              validationFunction: (value) {
+                if (value == null || value.isEmpty) {
+                  return "please enter your password";
+                } else if (value.length < 8) {
+                  return "password must be at least 8 characters long";
+                }
+              },
+              inputController: _passwordController,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Signup'),
             ),
           ],
         ),
