94e2e8fc906671d0751fcf3fcd97f5b0cead779b
Buteya <buteyamasira@gmail.com>
2024-07-17 20:11:44 +0300

    signupscreen design upgrade

--- lib/screens/auth/signupscreen.dart
+++ lib/screens/auth/signupscreen.dart
@@ -22,99 +22,108 @@ class _SignupScreenState extends State<SignupScreen> {
       body: SingleChildScrollView(
         child: Form(
           key: _formKey,
-          child: Card(
-            child: Container(
-              margin: EdgeInsets.all(30.0),
-              child: Column(
-                children: [
-                  const Card.filled(child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(Icons.car_rental_rounded,size: 96,color: Colors.deepPurpleAccent
-                      ,),
-                  ),),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Text(
-                      'Signup',
-                      style: GoogleFonts.lato(
-                        textStyle: Theme.of(context).textTheme.displayLarge,
-                        fontSize: 48,
-                        fontWeight: FontWeight.w700,
-                        fontStyle: FontStyle.italic,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              child: Container(
+                margin: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    const Card.filled(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.car_rental_rounded,
+                          size: 96,
+                          color: Colors.deepPurpleAccent,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Text(
-                      'Welcome to carmate, an application that helps you get the car you need at the press of a button.',
-                      style: GoogleFonts.roboto(
-                        fontSize: 17,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text(
+                        'Signup',
+                        style: GoogleFonts.lato(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 48,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                        ),
                       ),
                     ),
-                  ),
-                  FormInput(
-                    inputController: _usernameController,
-                    labelText: 'Username',
-                    hintText: 'Enter your username',
-                    validationFunction: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a username';
-                      }
-                      return null;
-                    },
-                    obscureText: false,
-                  ),
-                  FormInput(
-                    inputController: _emailController,
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                    validationFunction: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "please enter your email";
-                      } else if (!value.contains("@")) {
-                        return "please enter a valid email";
-                      }
-                      return null;
-                    },
-                    obscureText: false,
-                  ),
-                  FormInput(
-                    obscureText: false,
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    validationFunction: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "please enter your password";
-                      } else if (value.length < 8) {
-                        return "password must be at least 8 characters long";
-                      }
-                      return null;
-                    },
-                    inputController: _passwordController,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 24.0),
-                    child: ElevatedButton(
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text(
+                        'Welcome to carmate, an application that helps you get the car you need at the press of a button.',
+                        style: GoogleFonts.roboto(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    FormInput(
+                      inputController: _usernameController,
+                      labelText: 'Username',
+                      hintText: 'Enter your username',
+                      validationFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a username';
+                        }
+                        return null;
+                      },
+                      obscureText: false,
+                    ),
+                    FormInput(
+                      inputController: _emailController,
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      validationFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "please enter your email";
+                        } else if (!value.contains("@")) {
+                          return "please enter a valid email";
+                        }
+                        return null;
+                      },
+                      obscureText: false,
+                    ),
+                    FormInput(
+                      obscureText: false,
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      validationFunction: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "please enter your password";
+                        } else if (value.length < 8) {
+                          return "password must be at least 8 characters long";
+                        }
+                        return null;
+                      },
+                      inputController: _passwordController,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 24.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Validate will return true if the form is valid, or false if
+                          // the form is invalid.
+                          if (_formKey.currentState!.validate()) {
+                            // Process data.
+                          }
+                        },
+                        child: const Text('Signup'),
+                      ),
+                    ),
+                    TextButton(
                       onPressed: () {
-                        // Validate will return true if the form is valid, or false if
-                        // the form is invalid.
-                        if (_formKey.currentState!.validate()) {
-                          // Process data.
+                        if (mounted) {
+                          Navigator.pushNamed(context, '/login');
                         }
                       },
-                      child: const Text('Signup'),
+                      child:
+                          const Text("already have an account? login here!!!"),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (mounted) {
-                        Navigator.pushNamed(context, '/login');
-                      }
-                    },
-                    child: const Text("already have an account? login here!!!"),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
